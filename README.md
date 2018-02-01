11ebf620cc24a7c02c2d78972f9f5af039a67f60
Jake Swanson <jakswa@gmail.com>
2018-02-01 16:42:18 -0500

    initial commit (gutted create-react-app skeleton)

---
+++ README.md
@@ -0,0 +1,7 @@
+# marta.io UI
+
+Static website for marta.io.
+
+# create-react-app
+
+This still mostly the create-react-app boilerplate, some pieces gutted.
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>marta.io</h1>
+        </header>
+        <p>
+          Here we go.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
