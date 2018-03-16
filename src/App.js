7faee92f9cd41dfeeb8fec8c3f12296c5e02bfd1
Jake Swanson <jakswa@gmail.com>
2018-03-15 21:36:48 -0400

    remove stray CSS (now in public/index.html)
    i forgot it when fixing #8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import StationList from './StationList/StationList';
 import StationView from './StationView/StationView';
 import TrainView from './TrainView/TrainView';
-import './App.css';
 import React from "react";
 import Grid from "material-ui/Grid";
 import { BrowserRouter as Router, Route } from "react-router-dom";
