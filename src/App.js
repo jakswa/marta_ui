8db14e15ef8913e6729883c259eef7a06ab193f1
Zans Litinskis <Zans.Litinskis@vertumotors.com>
2019-09-30 18:58:02 +0100

    moving Appbar to StationList only and moving toggle themes to bottom of app & fixing icons

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 import StationList from './StationList/StationList';
 import StationView from './StationView/StationView';
 import TrainView from './TrainView/TrainView';
-import React from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // CSS
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import  AppThemeOptions  from './theme/theme'
-import { Switch, CssBaseline } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import AppThemeOptions from './theme/theme'
+import { CssBaseline } from '@material-ui/core';
 
-const App = ({ toggleTheme, theme }) => {
-  
-  const storedTheme = createMuiTheme(AppThemeOptions[theme]);
-  const handleChange = event => {
-    console.log('createing mui theme to ',theme );
-    toggleTheme(!theme)
+
+class App extends Component {
+  render() {
+    const { theme, toggleTheme } = this.props // drilling props( as we are not using Redux here or some state managment) for passing between components
+
+    const storedTheme = createMuiTheme(AppThemeOptions[theme]); // Picking up selecting theme
+
+    return (
+      <Router>
+        <MuiThemeProvider theme={storedTheme}>
+          <CssBaseline />
+          <Grid container justify="center" spacing={0}>
+            <Grid item xs={12} xl={4}>
+              <Route exact path="/" render={() => <StationList theme={theme} toggleTheme={toggleTheme} />} />
+              <Route path="/station/:station" component={StationView} />
+              <Route path="/train/:train_id" component={TrainView} />
+            </Grid>
+          </Grid>
+        </MuiThemeProvider>
+      </Router>
+    )
   }
-  return (
-  <Router>
-    <MuiThemeProvider theme={storedTheme}>
-      <CssBaseline/>
-    <Grid container justify="center" spacing={0}>
-      <Grid item xs={12} xl={4}>
-      <AppBar position="static" color="primary" elevation={0}>
-          <Toolbar>
-            <Typography variant="title" color="inherit">
-              Stations
-            </Typography>
-            <Switch onChange={handleChange}/>
-          </Toolbar>
-        </AppBar>
-
-        <Route exact path="/" render={()=><StationList theme={theme}/>}/>
-        <Route path="/station/:station" component={StationView} />
-        <Route path="/train/:train_id" component={TrainView} />
-      </Grid>
-    </Grid>
-    </MuiThemeProvider>
-  </Router>
-  )
 };
 
 export default App;
--- src/StationList/StationList.js
+++ src/StationList/StationList.js
@@ -1,9 +1,16 @@
+// npm packages
 import React, { Component } from 'react';
+// Material UI
+import { Switch } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import Avatar from '@material-ui/core/Avatar';
 import Icon from '@material-ui/core/Icon';
 import Button from '@material-ui/core/Button';
+// Components
 import StarredStations from '../StarredStations/StarredStations';
 import NearbyStations from '../NearbyStations/NearbyStations';
 import AllStations from '../AllStations/AllStations';
@@ -11,24 +18,44 @@ import AllStations from '../AllStations/AllStations';
 class StationList extends Component {
 
   render() {
-   const { theme } = this.props
-    var list = [];
+    const { theme, toggleTheme } = this.props // passing theme and function for toggling different themes
 
+    // Function for changing theme
+    const handleChange = event => {
+      toggleTheme(!theme)
+    }
+
+    // Function for Capitalization of first letter in string
+    const capitalizeFirstLetter = (string) => {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+
+    var list = [];
     list.push(<StarredStations key="starredStations" />);
     list.push(<NearbyStations key="nearbyStations" />);
     list.push(<AllStations key="allStations" />);
 
     return (
+
       <div className="StationList">
+        <AppBar position="static" color="primary" elevation={0} >
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Stations
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <List className={theme === 'light' ? 'StationListHolder StationListHolder--light' : 'StationListHolder StationListHolder--dark'}>{list}</List>
-        <Paper elevation={0} style={{justifyContent: 'space-around', display: 'flex', padding: '16px'}}>
+        <Paper elevation={0} style={{ justifyContent: 'space-around', display: 'flex', padding: '16px' }}>
+          <div className={theme === 'light' ? 'StationListTheme-text--light' : 'StationListTheme-text--dark'}>{`${capitalizeFirstLetter(theme)} Mode`}<Switch onChange={handleChange} /></div>
           <Button variant="fab" mini={true} onClick={() => window.location = "https://twitter.com/jakswa"}>
             <Avatar src="https://s.gravatar.com/avatar/721d6b5c0b5345637b76ea17318a447c?s=80&r=g" />
           </Button>
-          <Button style={{marginLeft: '16px'}} mini={true} variant="fab" color="secondary" onClick={() => window.location = "https://github.com/jakswa/marta_ui"}>
+          <Button style={{ marginLeft: '16px' }} mini={true} variant="fab" color="secondary" onClick={() => window.location = "https://github.com/jakswa/marta_ui"}>
             <Icon>code</Icon>
           </Button>
-          <Button style={{marginLeft: '16px'}} mini={true} variant="fab" onClick={() => window.location = "https://gitter.im/marta_ui/Lobby"}>
+          <Button style={{ marginLeft: '16px' }} mini={true} variant="fab" onClick={() => window.location = "https://gitter.im/marta_ui/Lobby"}>
             <Icon>message</Icon>
           </Button>
         </Paper>
