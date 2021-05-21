96aa49ee95d36957629159733e1d11a2d211fdc4
Jake Swanson <jakswa@gmail.com>
2021-05-21 16:33:28 -0400

    train timeline in use

--- src/TrainView/TrainView.js
+++ src/TrainView/TrainView.js
@@ -12,6 +12,17 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Box from '@material-ui/core/Box';
 
+// Timeline reqs
+import Stations from '../marta/stations';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import Paper from '@material-ui/core/Paper';
+
 class StationView extends Component {
   constructor(props) {
     super(props);
@@ -22,12 +33,11 @@ class StationView extends Component {
   }
 
   componentDidMount() {
-    // TODO: need more than just next two arrivals
     this.subscribeCallback = (arrivals) => {
       var matching = []
-      for(var i = 0; i < arrivals.length; i++) {
+      for (var i = 0; i < arrivals.length; i++) {
         var arrival = arrivals[i];
-        if(arrival.TRAIN_ID === this.state.trainID) {
+        if (arrival.TRAIN_ID === this.state.trainID) {
           matching.push(arrival);
         }
       }
@@ -49,37 +59,118 @@ class StationView extends Component {
       <div className="TrainView">
         <AppBar position="static" color="primary" elevation={0}>
           <Toolbar>
-            <IconButton onClick={this.goBack} color="default"><Icon style={{color: 'white'}}>arrow_back</Icon></IconButton>
+            <IconButton onClick={this.goBack} color="default"><Icon style={{ color: 'white' }}>arrow_back</Icon></IconButton>
             <Typography variant="h6" color="inherit">
               Train ID {this.state.trainID}
             </Typography>
           </Toolbar>
         </AppBar>
-        <List>{this.arrivals()}</List>
+        <Timeline className={`${this.lineColor()}-line`}>{this.arrivals()}</Timeline>
       </div>
     )
   }
 
+  lineColor() {
+    if (this._lineColor) return this._lineColor;
+    var nextArrival = this.state.arrivals[0];
+    this._lineColor = nextArrival && nextArrival.LINE.toLowerCase();
+    return this._lineColor;
+  }
+
+  trainDestination() {
+    if (this._trainDestination) return this._trainDestination;
+    var nextArrival = this.state.arrivals[0];
+    this._trainDestination = nextArrival && nextArrival.DESTINATION.toLowerCase().replace(' station', '');
+    return this._trainDestination;
+  }
+
+  // working around wonky API data flickering around here.
+  // A priority is UI stability. We'll grow the list of stations as
+  // they sit there reading, but we won't shrink it.
+  // This means if they visit during flaky JSON, it might show 1 entry,
+  // and if the API updates with 10 entries for the train, we'll flicker
+  // jarringly to keep the UI accurate, and grow the list. If 20s later
+  // the list shrinks back down to 1 entry, we will not shrink the list.
+  updateLineStations() {
+    var lineColor = this.lineColor();
+    var line = lineColor && Stations.LINES[lineColor].slice();
+    var nextArrival = this.state.arrivals[0];
+    if (line && nextArrival && (nextArrival.DIRECTION === 'N' || nextArrival.DIRECTION == 'W')) {
+      line.reverse();
+    }
+    var startInd = line && nextArrival && line.findIndex((station) => {
+      return nextArrival.STATION.toLowerCase().indexOf(station) === 0;
+    });
+    if (line && startInd > -1) line = line.slice(startInd);
+    if (line && (!this.lineStations || this.lineStations.length < line.length)) {
+      this.lineStations = line;
+    }
+  }
+
   arrivals() {
-    if (this.state.arrivals.length === 0) {
+    this.updateLineStations();
+    var stations = this.lineStations;
+
+    if (!stations) {
       return (
-        <ListItem divider key="empty">
-          <ListItemText primary="No data available" />
-        </ListItem>
+        <TimelineItem>
+          <TimelineContent>
+            <Typography>No data available</Typography>
+          </TimelineContent>
+        </TimelineItem>
       )
     }
+
     var res = [];
-    for(var i = 0; i < this.state.arrivals.length; i++) {
-      var arrival = this.state.arrivals[i];
-      var className = arrival.LINE + "Line";
+    for (var i = 0; i < stations.length; i++) {
+      var station = stations[i];
+      var arrival = this.state.arrivals.find((arr) => arr.STATION.toLowerCase().indexOf(station) > -1);
       res.push(
-        <ListItem button divider key={arrival.STATION}>
-          <Chip classes={{ root: className }} avatar={<Avatar className='Badge'>{arrival.DIRECTION}</Avatar>} label={arrival.STATION.replace(/ station$/i, '')} />
-          <Box mx={2}><ListItemText primary={arrival.WAITING_TIME} /></Box>
-        </ListItem>
+        <TimelineItem>
+          <TimelineOppositeContent>
+            <Typography>{station}</Typography>
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot>
+            </TimelineDot>
+            {this.connectorFor(i, stations.length)}
+          </TimelineSeparator>
+          <TimelineContent>
+            {this.contentFor(arrival, station)}
+          </TimelineContent>
+        </TimelineItem>
       );
     }
     return res;
+  }
+
+  contentFor(arrival, station) {
+    if (!arrival) return;
+
+    if (station === this.trainDestination()) {
+      return <Paper>
+        <Typography variant="h6" component="h1">
+          {arrival.WAITING_TIME}
+        </Typography>
+        <Typography>destination</Typography>
+      </Paper>;
+    }
+
+    if (arrival === this.state.arrivals[0]) {
+      return <Paper>
+        <Typography variant="h6" component="h1">
+          {arrival.WAITING_TIME}
+        </Typography>
+      </Paper>;
+    }
+
+    return <Typography>{arrival.WAITING_TIME}</Typography>;
+  }
+
+  connectorFor(i, len) {
+    if (i < len - 1) {
+      return <TimelineConnector />;
+    }
   }
 }
 
